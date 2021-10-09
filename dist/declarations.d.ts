b72b0615169f201f94da1b58aaefb9a336c2d18a
Osinakayah Ifeanyi <ifeanyiosinakayah15@gmail.com>
2021-10-09 13:25:04 +0100

    add dist folder (temp)

---
+++ dist/declarations.d.ts
@@ -0,0 +1,10 @@
+interface CoinWallet {
+    send: unknown;
+    enable: () => Promise<string[]>;
+    on?: (method: string, listener: (...args: any[]) => void) => void;
+    removeListener?: (method: string, listener: (...args: any[]) => void) => void;
+}
+declare interface Window {
+    eth?: CoinWallet;
+}
+declare const __DEV__: boolean;
